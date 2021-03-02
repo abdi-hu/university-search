91b5240139e96768ae5ce64bf0ee5289de1b1ac1
Abdisamad Hussein <abdisamadh91@gmail.com>
2021-03-02 13:44:56 -0500

    updated working js code

--- js/script.js
+++ js/script.js
@@ -1,29 +1,43 @@
-let univData;
+let univData = {};
 
 const $webPage = $('.webPage');
 const $country = $('.country');
 const $input = $('#input')
 const $name = $('.name')
+const $container = $('.container');
 
 
 const BASE_URL = 'http://universities.hipolabs.com/search?name='
 
+//submit event listener 
 $('form').on('submit', handleSubmit);
 
+//function run on every submit
 function handleSubmit(evt) {
     evt.preventDefault();
     let $searchTerm = $input.val()
     $.ajax(BASE_URL + $searchTerm).then(function (data) {
         univData = data;
-        console.log(data);
         render();
-
+        console.log(univData)
+        //If error is returned
     }, function (error) {
         console.log('Error ', error);
     })
 }
 function render() {
-    $webPage.text(univData[0].web_pages);
-    $name.text(univData[0].name);
-    $country.text(univData[0].country);
+    let row = ``
+    $.each(univData, (index, univ) => {
+        row += `
+        <div class="univ">
+        <h3 class="name">${univ.name}</h3>
+        <div>
+            <p class="webPage">${univ.web_pages}</p>
+            <p class="country">${univ.country}</p>
+        </div>
+        </div>
+        `;
+    });
+    $('.container').html(row);
 }
+
