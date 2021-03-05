a4a226c30983f45ca6ff757e86e413ecaaa3d132
Abdisamad Hussein <abdisamadhussein@Abdisamads-MacBook-Pro.local>
2021-03-05 09:22:22 -0500

    additional styling and update to ajax request

--- js/script.js
+++ js/script.js
@@ -1,42 +1,49 @@
-let univData = {};
+$(function () {
+    //Declaring variables
+    let univData = {};
 
-//
-const $webPage = $('.webPage');
-const $country = $('.country');
-const $input = $('#input')
-const $name = $('.name')
-const $container = $('#container');
-const $selCountry = $('#selCountry')
+    const $webPage = $('.webPage');
+    const $country = $('.country');
+    const $input = $('#input')
+    const $name = $('.name')
+    const $container = $('#container');
+    const $selCountry = $('#selCountry')
 
 
+    //Base url used with ajax request
+    const BASE_URL = 'https://universities.hipolabs.com/search?name='
 
-const BASE_URL = 'http://universities.hipolabs.com/search?name='
+    //Event listener for submiting the search
+    $('form').on('submit', handleSubmit);
 
-//submit event listener 
-$('form').on('submit', handleSubmit);
+    //function run on every submit
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        let $searchTerm = $input.val()
+        let $cnt = `&country=${$selCountry.val()}`
 
-//function run on every submit
-function handleSubmit(evt) {
-    evt.preventDefault();
-    let $searchTerm = $input.val()
-    let $cnt = `&country=${$selCountry.val()}`
+        //removes appended pagination on the subsequent search
+        $('.page-item').remove();
 
-    $.ajax(BASE_URL + $searchTerm + $cnt).then(function (data) {
-        univData = data;
-        //function to render the content on the page
-        render();
-        //pagination 
-        applyPagination();
-        //If error is returned
-    }, function (error) {
-        console.log('Error ', error);
-    })
-}
-function render() {
-    let row = ``
-    $.each(univData, (index, univ) => {
-        row +=
-            `
+        $.ajax(BASE_URL + $searchTerm + $cnt).then(function (data) {
+            univData = data;
+
+            //function to render the content on the page
+            render();
+            // Function to establish pagination 
+            applyPagination();
+            //If error is returned during the AJAX request
+        }, function (error) {
+            console.log('Error ', error);
+        })
+    }
+    // Function to render the content from the API onto the DOM
+    function render() {
+        //variable to create each university object using the JQuery each method
+        let univCard = ``
+        $.each(univData, (index, univ) => {
+            univCard +=
+                `
         <div class="card border-info mb-3" style="max-width: 20rem;">
             <div class="card-header">${univ.country}</div>
                 <div class="card-body">
@@ -46,59 +53,64 @@ function render() {
             </div>
         </div>
         `
-    });
-    $('#container').html(row);
-}
-
-function applyPagination() {
-    //establishing the amount of pages based on data returned
-    const numberOfUniv = $("#container .card").length;
-    const limitPerPage = 6;
-    //gt selector to hide page after the page limit is reached
-    $(`#container .card:gt("${limitPerPage - 1}")`).hide()
-    const totalPages = Math.round(numberOfUniv / limitPerPage)
-    $('.pagination').append(`<li class="page-item active current-page"><a class="page-link" href="#">1</a></li>`)
-
-    for (let i = 2; i <= totalPages; i++) {
-        $('.pagination').append(`<li class="page-item current-page"><a class="page-link" href="#">${i}</a></li>`)
+        });
+        // Each content pulled is appened to the 'container' div
+        $('#container').html(univCard);
     }
-    $('.pagination').append(`<li class="page-item next-page"><a class="page-link" href="#">&raquo;</a></li>`)
+    //Function for pagination 
+    function applyPagination() {
+        //Establishing the amount of pages based on data returned
+        const numberOfUniv = $("#container .card").length;
+        const limitPerPage = 6;
+        //gt selector to hide page after the page limit is reached
+        $(`#container .card:gt("${limitPerPage - 1}")`).hide()
+        const totalPages = Math.round(numberOfUniv / limitPerPage)
+        $('.pagination').append(`<li class="page-item disabled"><a class="page-link" href="#">&laquo;</a></li><li class="page-item active current-page"><a class="page-link" href="#">1</a></li>`)
 
-    $('.pagination li.current-page').on('click', function () {
-        if ($(this).hasClass('active')) {
-            return false;
-        } else {
-            var currentPage = $(this).index();
-            $('.pagination li').removeClass('active');
-            $(this).addClass('active');
-            $('#container .card').hide();
-
-            let currentTotal = limitPerPage * currentPage
-
-            for (let i = currentTotal - limitPerPage; i < currentTotal; i++) {
-                $(`#container .card:eq(${i})`).show();
-            }
+        for (let i = 2; i <= totalPages; i++) {
+            $('.pagination').append(`<li class="page-item current-page"><a class="page-link" href="#">${i}</a></li>`)
         }
+        $('.pagination').append(`<li class="page-item next-page"><a class="page-link" href="#">&raquo;</a></li>`)
 
-    });
-    $('.next-page').on('click', function () {
-        var currentPage = $('.pagination li.active').index();
-        if (currentPage === totalPages) {
-            return false;
-        } else {
-            currentPage++;
-            $('.pagination li').removeClass('active')
-            $('#container .card').hide();
+        $('.pagination li.current-page').on('click', function () {
+            if ($(this).hasClass('active')) {
+                return false;
+            } else {
+                var currentPage = $(this).index();
+                $('.pagination li').removeClass('active');
+                $(this).addClass('active');
+                $('#container .card').hide();
 
-            let currentTotal = limitPerPage * currentPage;
+                let currentTotal = limitPerPage * currentPage
 
-            for (let i = currentTotal - limitPerPage; i < currentTotal; i++) {
-                $(`#container .card:eq(${i})`).show();
+                for (let i = currentTotal - limitPerPage; i < currentTotal; i++) {
+                    $(`#container .card:eq(${i})`).show();
+                }
             }
-            $(`.pagination li.current-page:eq(${currentPage - 1})`).addClass('active');
-        }
 
-    });
-}
+        });
+        $('.next-page').on('click', function () {
+            var currentPage = $('.pagination li.active').index();
+            if (currentPage === totalPages) {
+                return false;
+            } else {
+                currentPage++;
+                $('.pagination li').removeClass('active')
+                $('#container .card').hide();
+
+                let currentTotal = limitPerPage * currentPage;
+
+                for (let i = currentTotal - limitPerPage; i < currentTotal; i++) {
+                    $(`#container .card:eq(${i})`).show();
+                }
+                $(`.pagination li.current-page:eq(${currentPage - 1})`).addClass('active');
+            }
+
+        });
+    }
 
 
+}); // IIFE to protect global scope
+
+
+{/* <li class="page-item disabled"><a class="page-link" href="#">&laquo;</a></li> */ }
